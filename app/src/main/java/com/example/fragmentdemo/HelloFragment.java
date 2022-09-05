199f8f601cdf90487de1dc8da1f138274ba4eb52
Jeova Henrique <jeovahenrique92@gmail.com>
2022-09-05 12:43:23 -0300

    fragmentProgram

--- app/src/main/java/com/example/fragmentdemo/HelloFragment.java
+++ app/src/main/java/com/example/fragmentdemo/HelloFragment.java
@@ -1,13 +1,13 @@
 package com.example.fragmentdemo;
 
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 public class HelloFragment extends Fragment {
 
--- app/src/main/java/com/example/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.fragmentdemo;
 
+import static com.example.fragmentdemo.R.id.container;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HelloFragment helloFragment = new HelloFragment();
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(container, helloFragment, "helloFragment");
+        transaction.commit();
     }
 }
